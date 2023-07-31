12f8fe083d4bd150307ecfd36726034f0ea637ca
Carien Caljouw <ccaljouw@f1r3s7.codam.nl>
2023-07-31 16:35:56 +0200

    c00 ex00

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -6,14 +6,25 @@
 /*   By: ccaljouw <ccaljouw@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/31 15:03:27 by ccaljouw      #+#    #+#                 */
-/*   Updated: 2023/07/31 15:37:16 by ccaljouw      ########   odam.nl         */
+/*   Updated: 2023/07/31 16:34:52 by ccaljouw      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv)
 {
-	std::cout << "hello world" << std::endl;
+	if (argc > 1)
+	{ 
+		// for()
+		// {
+			std::string input = argv[1];
+			std::transform(input.begin(), input.end(), input.begin(), toupper);
+		// }
+		std::cout << input << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	return 0;
 }
