c5be996dcfd8b1e2113d4ef86960b0bd906eb78e
Carien Caljouw <ccaljouw@f1r3s7.codam.nl>
2023-07-31 15:39:22 +0200

    start of c modules

---
+++ C00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   megaphone.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ccaljouw <ccaljouw@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/07/31 15:03:27 by ccaljouw      #+#    #+#                 */
+/*   Updated: 2023/07/31 15:37:16 by ccaljouw      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
