6a758265f533de2b7dd6d2c65c9f8d3d3769334c
ccaljouw <ccaljouw@student.codam.nl>
2024-08-16 16:21:26 +0200

    CPP09 ex02

--- C09/ex02/PmergeMe.cpp
+++ C09/ex02/PmergeMe.cpp
@@ -7,7 +7,7 @@ PmergeMe::PmergeMe() {}
 
 PmergeMe::PmergeMe(char **input) : _strangler(-1) {
  int i = 1;
- _begin = std::chrono::high_resolution_clock::now();
+ _init = std::chrono::high_resolution_clock::now();
  for (i = 1; input[i] ; i++) {
     try {
       int num = std::stoi(input[i]);
@@ -15,6 +15,7 @@ PmergeMe::PmergeMe(char **input) : _strangler(-1) {
         throw std::invalid_argument("not a positive number (" + std::string(input[i]) + ")");
       }
       _numbersToSort.push_back(num);
+      _numbersToSortList.push_back(num);
     } catch (std::out_of_range &e) {
       throw std::out_of_range("int overflow (" + std::string(input[i]) + ")");
     } catch (std::invalid_argument &e) {
@@ -24,6 +25,8 @@ PmergeMe::PmergeMe(char **input) : _strangler(-1) {
   if (i < 4) {
     throw std::invalid_argument("Not enough input to sort");
   }
+  generateJacobsthalSequence(_pendElements.size());
+  _inputParsed = std::chrono::high_resolution_clock::now();
 }
 
 PmergeMe::PmergeMe(PmergeMe const& rhs) {
@@ -40,78 +43,76 @@ PmergeMe& PmergeMe::operator=(PmergeMe const& rhs) {
   return *this;
 }
 
-size_t PmergeMe::getSize() const {
-  return _numbersToSort.size();
-}
-
-void PmergeMe::printNumbersToSort() const {
-  std::cout << "Before: ";
-  this->printVector(_numbersToSort);
-}
-
-void PmergeMe::clearChains() {
-  this->_mainChain.clear();
-  this->_pendElements.clear();
-}
-
-void PmergeMe::printSortedNumbers() const {
-  std::cout << "After: ";
-  this->printVector(_mainChain);
-}
-
-void PmergeMe::printVector(std::vector<int> const& vec) const {
-  for (std::vector<int>::const_iterator it = vec.begin(); it != vec.end(); it++) {
-    std::cout << *it;
-    if (it != vec.end() - 1) {
-      std::cout << ", ";
-    }
-  }
-  std::cout << std::endl;
-}
-
-std::vector<size_t> PmergeMe::generateJacobsthalSequence(size_t n) {
-  std::vector<size_t> jacobsthalSeq;
-  if (n < 1) return jacobsthalSeq;
-  jacobsthalSeq.push_back(0);
-  if (n < 2) return jacobsthalSeq;
-  jacobsthalSeq.push_back(1);
-  if (n < 3) return jacobsthalSeq;
+void PmergeMe::generateJacobsthalSequence(size_t n) {
+  if (n < 1) return;
+  _jacobsthalSeq.push_back(0);
+  if (n < 2) return;
+  _jacobsthalSeq.push_back(1);
+  if (n < 3) return;
   for (size_t i = 2; i < n; i++) {
-    jacobsthalSeq.push_back(jacobsthalSeq[i - 1] + 2 * jacobsthalSeq[i - 2]);
+    _jacobsthalSeq.push_back(_jacobsthalSeq[i - 1] + 2 * _jacobsthalSeq[i - 2]);
   }
   // return sequence starting from 3rd position as the first element (index 0) of the pend sequence has already been inserted
   // and the second and third element from the sequance are both 1
-  std::vector<size_t> result(jacobsthalSeq.begin() + 2, jacobsthalSeq.end());
-  return result;
+  _jacobsthalSeq.erase(_jacobsthalSeq.begin());
+  _jacobsthalSeq.erase(_jacobsthalSeq.begin());
 }
 
-// std::lower_bound is used to find the first position in the sorted vector where the given 
-// element can be inserted without violating the sorting order.
-void PmergeMe::insertElemetInMainChain(int element) {
- auto pos = std::lower_bound(_mainChain.begin(), _mainChain.end(), element);
-    _mainChain.insert(pos, element);
-}
-
-// 1. Create and compair pairs
-std::vector<std::pair<int, int>> PmergeMe::createAndComparePairs() {
+void PmergeMe::sortFordJohnson() {
+  _startVectorSort = std::chrono::high_resolution_clock::now();
   std::vector<std::pair<int, int>> pairVect;
+  this->createAndComparePairs(pairVect);
+  this->sortPairsByFirstElement(pairVect);
+  this->insertionSortVector();
+  _sortedVector = std::chrono::high_resolution_clock::now();
+  _startListSort = std::chrono::high_resolution_clock::now();
+  std::list<std::pair<int, int>> pairList;
+  this->createAndComparePairs(pairList);
+  this->sortPairsByFirstElement(pairList);
+  this->insertionSortList();
+  _sortedList = std::chrono::high_resolution_clock::now();
+}
+
+std::chrono::duration<double, std::milli> PmergeMe::getParsingTime() const {
+  return _inputParsed - _init;
+}
+
+void  PmergeMe::showProcess() const {
+  std::cout << "\n---------------------------------" << std::endl;
+  std::cout << "Time to parse input: " << this->getParsingTime().count() << " ms" << std::endl;
+  std::cout << "---------------------------------" << std::endl;
+  std::cout << "Before: ";
+  this->printSequence(_numbersToSort);
+  std::cout << "After: ";
+  this->printSequence(_mainChain);
+  std::cout << "---------------------------------" << std::endl;
+  std::cout << "Time to process a range of " << this->_numbersToSort.size() \
+      << " elements with [std::vector]\t" << this->getSortingTimeVector().count() <<" ms\n";
+  std::cout << "Time to process a range of " << this->_numbersToSortList.size() \
+      << " elements with [std::list]\t" << this->getSortingTimeList().count() <<" ms\n";
+  std::cout << "---------------------------------" << std::endl;
+}
+
+
+// VECTOR
+// 1. Create and compair pairs
+void  PmergeMe::createAndComparePairs(std::vector<std::pair<int, int>>& pairVect) {
   std::pair<int, int> tempPair;
 
   for (std::vector<int>::iterator it = _numbersToSort.begin(); it != _numbersToSort.end(); it++) {
     if (it + 1 != _numbersToSort.end()) {
-      tempPair = std::make_pair(std::max(*it, *(it + 1)), std::min(*it, *(it + 1)));
+      tempPair = std::make_pair( std::max(*it, *(it + 1)), std::min(*it, *(it + 1)));
       pairVect.push_back(tempPair);
       it++;
     } else {
       this->_strangler = *it;
     }
   }
-  return pairVect;
 }
 
 // 2. sort paris by first element and create main chain and pend
 // the first element of the second sequence can alreay be inserted at the front of the first sequence
-void PmergeMe::sortPairsByFirstElement(std::vector<std::pair<int, int>> pairVect) {
+void PmergeMe::sortPairsByFirstElement(std::vector<std::pair<int, int>>& pairVect) {
   if (pairVect.empty()) return;
 
   auto compare = [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
@@ -137,50 +138,92 @@ void PmergeMe::sortPairsByFirstElement(std::vector<std::pair<int, int>> pairVect
   }
 }
 
-// 3. Insertion sort using Jacobsthal sequence
-void PmergeMe::insertionSortUsingJacobsthal() {
-  std::vector<size_t> jacobsthal = generateJacobsthalSequence(_pendElements.size());
+// std::lower_bound is used to find the first position in the sorted vector where the given 
+// element can be inserted without violating the sorting order.
+void PmergeMe::insertElemetInMainChainVector(int element) {
+ auto pos = std::lower_bound(_mainChain.begin(), _mainChain.end(), element);
+    _mainChain.insert(pos, element);
+}
 
-    for (size_t index : jacobsthal) {
+// 3. Insertion sort using Jacobsthal sequence
+void PmergeMe::insertionSortVector() {
+
+    for (size_t index : _jacobsthalSeq) {
       if (index < _pendElements.size()) {
-        insertElemetInMainChain(_pendElements[index]);
+        insertElemetInMainChainVector(_pendElements[index]);
       }
     }
     // Insert remaining elements that were not included in the Jacobsthal sequence
     for (size_t i = 0; i < _pendElements.size(); ++i) {
-      if (std::find(jacobsthal.begin(), jacobsthal.end(), i) == jacobsthal.end()) {
-        insertElemetInMainChain(_pendElements[i]);
+      if (std::find(_jacobsthalSeq.begin(), _jacobsthalSeq.end(), i) == _jacobsthalSeq.end()) {
+        insertElemetInMainChainVector(_pendElements[i]);
       }
     }
 }
 
-void PmergeMe::sortFordJohnson() {
-  this->clearChains();
-  _inputParsed = std::chrono::high_resolution_clock::now();
-  // 1. generate pairs and put largest number first
-  std::vector<std::pair<int, int>> tempVect = this->createAndComparePairs();
-
-  // 2. sort pairs by first element and create 2 seperate sequences
-  this->sortPairsByFirstElement(tempVect);
-
-  // 3. perform insertion sort based on Jacobsthal sequence
-  this->insertionSortUsingJacobsthal();
-  _sorted = std::chrono::high_resolution_clock::now();
+std::chrono::duration<double, std::milli> PmergeMe::getSortingTimeVector() const {
+  return _sortedVector - _startVectorSort;
 }
 
-std::chrono::duration<double, std::milli> PmergeMe::getParsingTime() const {
-  return _inputParsed - _begin;
+// LIST
+void  PmergeMe::createAndComparePairs(std::list<std::pair<int, int>>& pairList) {
+  if (_numbersToSortList.empty()) return; 
+
+  auto it = _numbersToSortList.begin();
+  while (std::next(it) != _numbersToSortList.end()) {
+    auto next_it = std::next(it);
+    std::pair<int, int> tempPair = std::make_pair(std::max(*it, *next_it), std::min(*it, *next_it));
+    pairList.push_back(tempPair);
+    ++it;
+    ++it;
+  }
+  if (it != _numbersToSortList.end()) {
+    this->_strangler = *it;
+  }
 }
 
-std::chrono::duration<double, std::milli> PmergeMe::getSortingTime() const {
-  return _sorted - _inputParsed;
+void  PmergeMe::sortPairsByFirstElement(std::list<std::pair<int, int>>& pairList) {
+  pairList.sort([](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+    return a.first < b.first;
+  });
+  _mainChainList.push_back(pairList.front().second);
+  _mainChainList.push_back(pairList.front().first);
+  for (auto it = std::next(pairList.begin()); it != pairList.end(); ++it) {
+    _mainChainList.push_back(it->first);
+    _pendElementsList.push_back(it->second);
+  }
+
+  if (_strangler != -1) {
+      _pendElementsList.push_back(_strangler);
+  }
 }
 
-void  PmergeMe::showProcess() const {
-  this->printNumbersToSort();
-  this->printSortedNumbers();
-  std::cout << "Time to parse input: " << this->getParsingTime().count() << " ms" << std::endl;
-  std::cout << "Time to process a range of " << this->getSize() \
-      << " elements with std::vector : " << this->getSortingTime().count() <<" ms" << std::endl;
+void PmergeMe::insertionSortList() {
+  auto pendIt = _pendElementsList.begin();
+  for (size_t index : _jacobsthalSeq) {
+    std::advance(pendIt, index);
+    if (pendIt != _pendElementsList.end()) {
+        insertElemetInMainChainList(*pendIt);
+    }
+    pendIt = _pendElementsList.begin();
+  }
+
+  for (size_t i = 0; i < _pendElementsList.size(); ++i) {
+    if (std::find(_jacobsthalSeq.begin(), _jacobsthalSeq.end(), i) == _jacobsthalSeq.end()) {
+        std::advance(pendIt, i);
+        insertElemetInMainChainList(*pendIt);
+    }
+  }
 }
 
+void  PmergeMe::insertElemetInMainChainList(int element) {
+  auto pos = _mainChainList.begin();
+  while (pos != _mainChainList.end() && *pos < element) {
+    ++pos;
+  }
+  _mainChainList.insert(pos, element);
+}
+
+std::chrono::duration<double, std::milli> PmergeMe::getSortingTimeList() const {
+  return _sortedList - _startListSort;
+}
--- C09/ex02/PmergeMe.hpp
+++ C09/ex02/PmergeMe.hpp
@@ -7,46 +7,66 @@
 #include <list>
 
 class PmergeMe {
+
   public:
+
     PmergeMe(char **input);
     PmergeMe(PmergeMe const& rhs);
     virtual ~PmergeMe();
 
     PmergeMe& operator=(PmergeMe const& rhs);
 
-    size_t  getSize() const;
-    std::chrono::duration<double, std::milli> getParsingTime() const;
-    std::chrono::duration<double, std::milli> getSortingTime() const;
-  
-    
     void    showProcess() const;
     void    sortFordJohnson();
 
   private:
-    std::vector<int>  _numbersToSort;
-    std::vector<int>  _mainChain;
-    std::vector<int>  _pendElements;
-    int               _strangler;
 
-    std::chrono::high_resolution_clock::time_point _begin;
+    int                                            _strangler;
+    std::vector<size_t>                            _jacobsthalSeq;
+    std::chrono::high_resolution_clock::time_point _startListSort;
+    std::chrono::high_resolution_clock::time_point _sortedList;    
+
+    std::chrono::duration<double, std::milli> getParsingTime() const;
+    void                                      generateJacobsthalSequence(size_t n);
+  
+    template <typename T>
+    void  printSequence(T const& seq) const {
+      for (const auto& element : seq) {
+          std::cout << element << " ";
+      }
+      std::cout << std::endl;
+    }
+
+  // vector
+    std::vector<int>                               _numbersToSort;
+    std::vector<int>                               _mainChain;
+    std::vector<int>                               _pendElements;
+    std::chrono::high_resolution_clock::time_point _startVectorSort;
+    std::chrono::high_resolution_clock::time_point _sortedVector;
+
+    std::chrono::duration<double, std::milli>      getSortingTimeList() const;
+
+    void  createAndComparePairs(std::vector<std::pair<int, int>>& pairVect);
+    void  sortPairsByFirstElement(std::vector<std::pair<int, int>>& pairVect);
+    void  insertionSortVector();
+    void  insertElemetInMainChainVector(int element);
+
+  
+  // list
+    std::list<int>                                 _numbersToSortList;
+    std::list<int>                                 _mainChainList;
+    std::list<int>                                 _pendElementsList;
+    std::chrono::high_resolution_clock::time_point _init;
     std::chrono::high_resolution_clock::time_point _inputParsed;
-    std::chrono::high_resolution_clock::time_point _sorted;
 
-    void    clearChains();
-    void    printVector(std::vector<int> const& vec) const;
-    void    printNumbersToSort() const;
-    void    printSortedNumbers() const;
+    std::chrono::duration<double, std::milli>      getSortingTimeVector() const;
 
-    std::vector<size_t>              generateJacobsthalSequence(size_t n);
-    std::vector<std::pair<int, int>> createAndComparePairs();
-    void                             sortPairsByFirstElement(std::vector<std::pair<int, int>> pairVect);
-    void                             insertionSortUsingJacobsthal();
-    void                             insertElemetInMainChain(int element);
-
+    void  createAndComparePairs(std::list<std::pair<int, int>>& pairList);
+    void  sortPairsByFirstElement(std::list<std::pair<int, int>>& pairList);
+    void  insertionSortList();
+    void  insertElemetInMainChainList(int element);
 
     PmergeMe();
 };
-
-std::ostream& operator<<(std::ostream &out, PmergeMe const &obj);
 
 #endif
